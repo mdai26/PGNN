a1ec0dbe357535f726c397a4320dd8ee0407affa
Minyi Dai <45828456+mdai26@users.noreply.github.com>
2022-04-01 17:25:32 -0500

    Added normalization

--- CNNbaseline/CNNdatazip.py
+++ CNNbaseline/CNNdatazip.py
@@ -8,31 +8,31 @@ Created on Wed Feb  9 23:09:27 2022
 # This code saves data into npz file
 import numpy as np
 import os
+import argparse
 
-def loaddata(group, numdata, Nx, Ny, Nz):
-    for g in range(1, group+1):
-        # read conductivity file first
-        foldername = '%d' % g
-        condfile = os.path.join(foldername, 'finalconductivity.txt')
-        gcond, gbcond, calcond = readcond(condfile, numdata)
-        # go through data points
-        for n in range(numdata):
-            # read structure file
-            fstruct = '%s/struct_%d.in' % (foldername, n)
-            struct = readstruct(fstruct, Nx, Ny, Nz)
-            # read euler angle
-            feulerang = '%s/eulerAng_%d.in' % (foldername, n)
-            eulerang = readeulerang(feulerang, Nx, Ny, Nz)
-            # get the conductivity matrix according to the structure id
-            conductivity = getcond(struct,gcond[n,:],gbcond[n,:])
-            # combine the conductivity matrix and the euler angle matrix
-            gimage = getimage(conductivity, eulerang)
-            if (g == 1) and (n == 0):
-                gimagelist, targetlist = [gimage],[calcond[n,:]]
-            else:
-                gimagelist, targetlist = np.concatenate((gimagelist, [gimage])), \
-                                         np.concatenate((targetlist, [calcond[n,:]]))
-            print('finish read data %d from group %d' % (n, g), flush = True)
+def loaddata(group, numdata, Nx, Ny, Nz, upperlimit, lowerlimit):
+    # read conductivity file first
+    foldername = '%d' % group
+    condfile = os.path.join(foldername, 'finalconductivity.txt')
+    gcond, gbcond, calcond = readcond(condfile, numdata, upperlimit, lowerlimit)
+    # go through data points
+    for n in range(numdata):
+        # read structure file
+        fstruct = '%s/struct_%d.in' % (foldername, n)
+        struct = readstruct(fstruct, Nx, Ny, Nz)
+        # read euler angle
+        feulerang = '%s/eulerAng_%d.in' % (foldername, n)
+        eulerang = readeulerang(feulerang, Nx, Ny, Nz)
+        # get the conductivity matrix according to the structure id
+        conductivity = getcond(struct,gcond[n,:],gbcond[n,:])
+        # combine the conductivity matrix and the euler angle matrix
+        gimage = getimage(conductivity, eulerang)
+        if n == 0:
+            gimagelist, targetlist = [gimage],[calcond[n,:]]
+        else:
+            gimagelist, targetlist = np.concatenate((gimagelist, [gimage])), \
+                    np.concatenate((targetlist, [calcond[n,:]]))
+        print('finish read data %d from group %d' % (n, group), flush = True)
 
     # change data to numpy array
     gimagelist = np.array(gimagelist)
@@ -40,11 +40,11 @@ def loaddata(group, numdata, Nx, Ny, Nz):
 
     return gimagelist, targetlist
 
-def readcond(condfile,numdata):
+def readcond(condfile,numdata, upperlimit, lowerlimit):
     # conductivity from file
-    cond = np.loadtxt(condfile)
+    cond = normcond(np.loadtxt(condfile),upperlimit, lowerlimit)
     gcond = np.copy(cond[:numdata,0:3])
-    gbcond = np.copy(cond[:numdata,3:6])
+    gbcond = npi.copy(cond[:numdata,3:6])
     calcond = np.copy(cond[:numdata,6:9])
     return gcond, gbcond, calcond
 
@@ -64,7 +64,7 @@ def readeulerang(feulerang, Nx, Ny, Nz):
     # get the euler angle data
     eulerang = np.zeros((Nx, Ny, Nz,3))
     for i in range(len(data)):
-        eulerang[int(data[i,0]-1), int(data[i,1]-1), int(data[i,2]-1),0:3] = data[i,3:6]
+        eulerang[int(data[i,0]-1), int(data[i,1]-1), int(data[i,2]-1),0:3] = data[i,3]/360, data[i,4]/180, data[i,5]/360
     
     return eulerang
 
@@ -86,16 +86,31 @@ def getimage(cond, eulerang):
     return gimage
 
 
-numdata = 100
-group = 10
-Nx = 64; Ny = 64; Nz = 64
-gimagelist, targetlist = loaddata(group, numdata, Nx, Ny, Nz)
-np.savez_compressed("CNNdata.npz", image = gimagelist, target = targetlist)
+def normcond(cond, upperlimit, lowerlimit):
+    cond = np.log(cond)
+    cond = (cond - lowerlimit) / (upperlimit - lowerlimit)
 
-#CNNdata = np.load("CNNdata.npz")
-#imagelist = CNNdata['image']
-#targetlist = CNNdata['target']
-#print("image size: ",imagelist.shape)
-#print("target size: ",targetlist.shape)
+    return cond
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--group', type = int, default = 1)
+    parser.add_argument('--numdata', type = int, default = 100)
+    parser.add_argument('--upperlimit', type = int, default = -2)
+    parser.add_argument('--lowerlimit',type = int, default = -8)
+    given_args = parser.parse_args()
+    group = given_args.group
+    numdata = given_args.numdata
+
+    Nx = 64; Ny = 64; Nz = 64
+    gimagelist, targetlist = loaddata(group, numdata, Nx, Ny, Nz, upperlimit, lowerlimit)
+    ofilename = 'CNNdata_%d.npz' % group
+    np.savez_compressed(ofilename, image = gimagelist, target = targetlist)
+
+    CNNdata = np.load(ofilename)
+    imagelist = CNNdata['image']
+    targetlist = CNNdata['target']
+    print("image size: ",imagelist.shape)
+    print("target size: ",targetlist.shape)
 
 
